ac4bbe5fdcb2b476f26ecd264d965d8d09c05780
Patryk.Mazurek <220981@edu.p.lodz.pl>
2020-03-10 18:27:22 +0100

    creating Money class and overwriting methods

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -5,8 +5,20 @@ public class Money {
     private int value;
     private String currency;
 
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
     public String getTotalCostCurrency() {
         return currency;
+    }
+
+    public int getValue() {
+        return value;
     }
 
 
